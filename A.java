c09266eb866ca6bb733d9f40acf1caed1aa6f99b
daduremohit12 <daduremohit12@gmail.com>
2023-03-30 17:30:28 +0530

    A.java modified

--- A.java
+++ A.java
@@ -2,7 +2,9 @@ public class A
 {
   public void m1()
   {
-    System.out.println("This is GitHub Demo class")
+    System.out.println("This is GitHub Demo class");
+    System.out.println("Status unchanged");
+    
   }
 
 
